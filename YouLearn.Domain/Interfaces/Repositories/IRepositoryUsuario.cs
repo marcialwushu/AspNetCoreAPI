1fab0e400a7e9e155667e1eab7ce4671fa3b77ae
cleilsonsousapereira <cleilsonpereira@unigrande.edu.br>
2020-07-30 17:18:36 -0300

    Criando serviços de dominio

--- YouLearn.Domain/Interfaces/Repositories/IRepositoryUsuario.cs
+++ YouLearn.Domain/Interfaces/Repositories/IRepositoryUsuario.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using YouLearn.Domain.Entities;
 
 namespace YouLearn.Domain.Interfaces.Repositories
 {
-    class IRepositoryUsuario
+    public interface IRepositoryUsuario
     {
+        Usuario Obter(Guid id);
+
+        Usuario Obter(string email, string senha);
+
+        void Salvar(Usuario usuario);
+
+        bool Existe(string email);
     }
 }
---
+++ YouLearn.Domain/Services/ServiceUsuario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YouLearn.Domain.Arguments.Usuario;
+using YouLearn.Domain.Entities;
+using YouLearn.Domain.Interfaces.Services;
+
+namespace YouLearn.Domain.Services
+{
+    public class ServiceUsuario : IServiceUsuario
+    {
+        public AdicionarUsuarioResponse AdicionarUsuario(AdicionarUsuarioRequest request)
+        {
+            if (request==null)
+            {
+                throw new Exception("Objeto AdicionarUsuarioRequest é obrigatório.");
+            }
+
+            Usuario usuario = new Usuario();
+            usuario.Nome.PrimeiroNome = "Paulo Rogério";
+            usuario.Nome.UltimoNome = "Martins Marques";
+            usuario.Email.Endereco = "paulo.analista@outlook.com";
+            usuario.Senha = "123456";
+
+            if (usuario.Nome.PrimeiroNome.Length < 3 || usuario.Nome.PrimeiroNome.Length > 50)
+            {
+                throw new Exception("Primeiro nome é obrigatório e deve conter entre 3 a 50 caracteres");
+            }
+
+            if (usuario.Nome.UltimoNome.Length < 3 || usuario.Nome.UltimoNome.Length > 50)
+            {
+                throw new Exception("Ultimo nome é obrigatório e deve conter entre 3 a 50 caracteres");
+            }
+
+            if (usuario.Email.Endereco.IndexOf('@') < 1)
+            {
+                throw new Exception("Email invalido");
+            }
+        }
+
+        public AutenticarUsuarioResponse AutenticarUsuario(AutenticarUsuarioRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
