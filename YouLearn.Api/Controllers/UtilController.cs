b5ca1b2df3702f9c70b07c50323188ab43f95739
Cleilson <marcialwushu@gmail.com>
2020-08-05 18:27:32 -0300

    Entendedno os verbos de nossa API

---
+++ YouLearn.Api/Controllers/UtilController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YouLearn.Api.Controllers
+{
+    public class UtilController
+    {
+        [HttpGet]
+        [Route("")]
+        public string Get()
+        {
+            return "Seja bem vindo ao YouLearn";
+        }
+
+        [HttpGet]
+        [Route("Versao")]
+        public string Versao()
+        {
+            return "0.0.1";
+        }
+
+        //[HttpPost]
+        //[Route("")]
+        //public string Post()
+        //{
+        //    return "Post - inserir nova informação";
+        //}
+
+        //[HttpPut]
+        //[Route("")]
+        //public string Put()
+        //{
+        //    return "Put - Atualizar informação";
+        //}
+
+        //[HttpDelete]
+        //[Route("")]
+        //public string Delete()
+        //{
+        //    return "Delete - Exclui informação";
+        //}
+    }
+}
