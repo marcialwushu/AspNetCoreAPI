3f16eaefba93ac2e64bfcaa0e4c048b36820080f
Cleilson <marcialwushu@gmail.com>
2020-08-09 17:14:05 -0300

    Blindadno classe da Entidade Video

--- YouLearn.Domain/Entities/Video.cs
+++ YouLearn.Domain/Entities/Video.cs
@@ -1,26 +1,53 @@
-﻿using YouLearn.Domain.Entities.Base;
+﻿using prmToolkit.NotificationPattern;
+using YouLearn.Domain.Entities.Base;
 using YouLearn.Domain.Enums;
 
 namespace YouLearn.Domain.Entities
 {
     public class Video : EntityBase
     {
-        public Canal Canal { get; set; }
+        public Video(Canal canal, PlayList playList, string titulo, string descricao, string tags, int ordemNaPlayList, string idVideoYoutube, Usuario usuarioSugeriu)
+        {
+            Canal = canal;
+            PlayList = playList;
+            Titulo = titulo;
+            Descricao = descricao;
+            Tags = tags;
+            OrdemNaPlayList = ordemNaPlayList;
+            IdVideoYoutube = idVideoYoutube;
+            UsuarioSugeriu = usuarioSugeriu;
+            Status = EnumStatus.EmAnalise;
 
-        public PlayList PlayList { get; set; }
+            new AddNotifications<Video>(this)
+                .IfNullOrInvalidLength(x => x.Titulo, 1, 200, "Titulo obrigatório e deve conter de 1 a 200 caracteres")
+                .IfNullOrInvalidLength(x => x.Descricao, 1, 250, "Descricao obrigatória e deve conter entre 1 a 50 caractares")
+                .IfNullOrInvalidLength(x => x.Tags, 1, 50, "Tag obrigatória e deve conter enre 1 a 50 caracateres")
+                .IfNullOrInvalidLength(x => x.IdVideoYoutube, 1, 50, "IdVideoYoutube é obrigatório e deve conter entre 1 a 50 caracteres");
 
-        public string Titulo { get; set; }
+            AddNotifications(canal);
 
-        public string Descricao { get; set; }
+            if(playList != null)
+            {
+                AddNotifications(playList);
+            }
+        }
 
-        public string Tags { get; set; }
+        public Canal Canal { get; private set; }
 
-        public int OrdemNaPlayList { get; set; }
+        public PlayList PlayList { get; private set; }
 
-        public string IdVideoYoutube { get; set; }
+        public string Titulo { get; private set; }
 
-        public Usuario UsuarioSugeriu { get; set; }
+        public string Descricao { get; private set; }
 
-        public EnumStatus Status { get; set; }
+        public string Tags { get; private set; }
+
+        public int OrdemNaPlayList { get; private set; }
+
+        public string IdVideoYoutube { get; private set; }
+
+        public Usuario UsuarioSugeriu { get; private set; }
+
+        public EnumStatus Status { get; private set; }
     }
 }
---
+++ YouLearn.Domain/Services/ServiceVideo.cs
@@ -0,0 +1,41 @@
+﻿using prmToolkit.NotificationPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YouLearn.Domain.Arguments.Base;
+using YouLearn.Domain.Arguments.Canal;
+using YouLearn.Domain.Arguments.Usuario;
+using YouLearn.Domain.Arguments.Video;
+using YouLearn.Domain.Entities;
+using YouLearn.Domain.Interfaces.Repositories;
+using YouLearn.Domain.Interfaces.Services;
+using YouLearn.Domain.ValueObjects;
+using Response = YouLearn.Domain.Arguments.Base.Response;
+
+namespace YouLearn.Domain.Services
+{
+    public class ServiceVideo : Notifiable, IServiceVideo
+    {
+        public AdicionarVideoResponse AddicionarVideo(AdicionarVideoRequest request, Guid idUsuario)
+        {
+            if(request == null)
+            {
+                AddNotification("AdicionarVideoRequest", "O Objeto AdicionarVideoRequest é Obrigatório");
+                return null;
+            }
+
+            Usuario usuario = _repositoryUsuario.Obter(idUsuario);
+        }
+
+        public IEnumerable<VideoResponse> Listar(string tags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<VideoResponse> Listar(Guid idPlayList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
