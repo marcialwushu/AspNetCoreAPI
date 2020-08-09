edd5a578c20c174de6f44c30cdfb056a75297164
Cleilson <marcialwushu@gmail.com>
2020-08-09 16:34:04 -0300

    inico implementacao service e repositorio video - interface e arguments

---
+++ YouLearn.Domain/Arguments/Video/AdicionarVideoResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouLearn.Domain.Arguments.Video
+{
+    public class AdicionarVideoResponse
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ YouLearn.Domain/Interfaces/Services/IServiceVideo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YouLearn.Domain.Arguments.Base;
+using YouLearn.Domain.Arguments.Canal;
+using YouLearn.Domain.Arguments.Usuario;
+using YouLearn.Domain.Interfaces.Services.Base;
+
+namespace YouLearn.Domain.Interfaces.Services
+{
+    public interface IServiceVideo : IServiceBase
+    {
+        AdicionarVideoResponse AddicionarVideo(AdicionarVideoRequest request, Guid idUsuario);
+    }
+}
