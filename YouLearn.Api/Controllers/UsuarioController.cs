97514c1dbea1298efda8744a966a3b8eec1c5bf5
Cleilson <marcialwushu@gmail.com>
2020-08-17 17:09:58 -0300

    Autenticao usuarioController
    Error: BadRequest - No suitable constructor found for entity type Canal Excecao gerada SystemInvalidOperationException em System.PrivateCoreLib.dll

--- YouLearn.Api/Controllers/UsuarioController.cs
+++ YouLearn.Api/Controllers/UsuarioController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using YouLearn.Api.Security;
 using YouLearn.Domain.Arguments.Usuario;
@@ -20,6 +25,7 @@ namespace YouLearn.Api.Controllers
             _serviceUsuario = serviceUsuario;
         }
 
+        [AllowAnonymous]
         [HttpPost]
         [Route("api/v1/Usuario/Adicionar")]
         public IActionResult Adicionar([FromBody]AdicionarUsuarioRequest request)
@@ -49,6 +55,53 @@ namespace YouLearn.Api.Controllers
             AutenticarUsuarioResponse response = _serviceUsuario.AutenticarUsuario(request);
 
             credenciaisValidas = response != null;
+
+            if (credenciaisValidas)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    new GenericIdentity(response.Id.ToString(), "Id"),
+                    new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        new Claim("Usuario", JsonConvert.SerializeObject(response))
+                    }
+                );
+
+                DateTime dataCriacao = DateTime.Now;
+                DateTime dataExpiracao = dataCriacao +
+                    TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    //Issuer = tokenConfigurations.Issuer,
+                    //Audience = tokenConfigurations.Audience,
+                    SigningCredentials = signingConfigurations.SigningCredentials,
+                    Subject = identity,
+                    NotBefore = dataCriacao,
+                    Expires = dataExpiracao
+
+                });
+                var token = handler.WriteToken(securityToken);
+
+                return new
+                {
+                    authenticated = true,
+                    created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    accessToken = token,
+                    message = "OK",
+                    PrimeiroNome = response.PrimeiroNome
+                };
+            }
+            else
+            {
+                return new
+                {
+                    authenticated = false,
+                    _serviceUsuario.Notifications
+                };
+            }
         }
     }
 }
